e9269c864288a3fad1ce0d0a4cdbd282c46b13fb
Nguyen Thanh Nghia <37038823+nghia89@users.noreply.github.com>
2022-12-20 23:00:11 +0700

    Setup API CRUD

---
+++ aspnet-core/src/ABPEcommerce.Admin.Application.Contracts/ProductCategories/IProductCategoriesAppService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace ABPEcommerce.Admin.ProductCategories
+{
+    public interface IProductCategoriesAppService : ICrudAppService
+        <ProductCategoryDto,
+        Guid,
+        PagedResultRequestDto,
+        CreateUpdateProductCategoryDto,
+        CreateUpdateProductCategoryDto>
+    {
+    }
+}
+
---
+++ aspnet-core/src/ABPEcommerce.Admin.Application/ProductCategories/ProductCategoriesAppService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ABPEcommerce.ProductCategoties;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace ABPEcommerce.Admin.ProductCategories
+{
+    public class ProductCategoriesAppService : CrudAppService<
+        ProductCategory,
+        ProductCategoryDto,
+        Guid,
+        PagedResultRequestDto,
+        CreateUpdateProductCategoryDto,
+        CreateUpdateProductCategoryDto>, IProductCategoriesAppService
+    {
+        public ProductCategoriesAppService(IRepository<ProductCategory, Guid> repository)
+            : base(repository)
+        {
+        }
+    }
+}
+
