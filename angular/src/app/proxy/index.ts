8b4d5ec50369be86f8e96ea1daa11e94fd03ece5
Nguyen Thanh Nghia <37038823+nghia89@users.noreply.github.com>
2022-12-25 21:36:32 +0700

    generate proxy client angular

---
+++ angular/src/app/proxy/index.ts
@@ -0,0 +1,5 @@
+import * as Abpecommerce from './abpecommerce';
+import * as ProductCategories from './product-categories';
+import * as Products from './products';
+export * from './models';
+export { Abpecommerce, ProductCategories, Products };
