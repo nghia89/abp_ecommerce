45c471b6d818ec78d9365bf947c008a5f0ca8bf6
Nguyen Thanh Nghia <37038823+nghia89@users.noreply.github.com>
2023-01-02 16:52:11 +0700

    validtion from product

--- angular/src/app/product/product.module.ts
+++ angular/src/app/product/product.module.ts
@@ -16,6 +16,7 @@ import { InputNumberModule } from 'primeng/inputnumber';
 import { CheckboxModule } from 'primeng/checkbox';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { EditorModule } from 'primeng/editor';
+import { AppSharedModule } from '@share/modules/app-share-module';
 @NgModule({
   declarations: [ProductComponent, ProductDetailComponent],
   imports: [
@@ -33,7 +34,8 @@ import { EditorModule } from 'primeng/editor';
     InputNumberModule,
     CheckboxModule,
     InputTextareaModule,
-    EditorModule
+    EditorModule,
+    AppSharedModule
   ],
   entryComponents: [
     ProductDetailComponent
