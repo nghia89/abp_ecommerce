178a313a671ce0156125f477b9651da1a756b44d
Nguyen Thanh Nghia <37038823+nghia89@users.noreply.github.com>
2023-02-14 22:51:14 +0700

    Sử dụng Background Worker với ABP

---
+++ aspnet-core/ABPEcommerce.Worker/ABPEcommerceBackgroundWorkersHostedService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+
+namespace ABPEcommerce.Worker
+{
+    public class ABPEcommerceBackgroundWorkersHostedService : IHostedService
+    {
+        private readonly IAbpApplicationWithExternalServiceProvider _application;
+        private readonly IServiceProvider _serviceProvider;
+
+        public ABPEcommerceBackgroundWorkersHostedService(
+            IAbpApplicationWithExternalServiceProvider application,
+            IServiceProvider serviceProvider)
+        {
+            _application = application;
+            _serviceProvider = serviceProvider;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _application.Initialize(_serviceProvider);
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _application.Shutdown();
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ aspnet-core/ABPEcommerce.Worker/ABPEcommerceBackgroundWorkersModule.cs
@@ -0,0 +1,51 @@
+﻿using ABPEcommerce.Worker.MailCampaigns;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Caching;
+using Volo.Abp.Modularity;
+using Volo.Abp;
+using Volo.Abp.Autofac;
+using Volo.Abp.BackgroundWorkers;
+using Volo.Abp.Caching.StackExchangeRedis;
+using ABPEcommerce.EntityFrameworkCore;
+using Microsoft.AspNetCore.DataProtection;
+
+namespace ABPEcommerce.Worker
+{
+    [DependsOn(
+     typeof(AbpAutofacModule),
+     typeof(AbpBackgroundWorkersModule),
+     typeof(AbpCachingStackExchangeRedisModule)
+ )]
+    public class TeduEcommerceBackgroundWorkersModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            var configuration = context.Services.GetConfiguration();
+            var hostEnvironment = context.Services.GetSingletonInstance<IHostEnvironment>();
+
+            context.Services.AddHostedService<ABPEcommerceBackgroundWorkersHostedService>();
+
+            Configure<AbpDistributedCacheOptions>(options =>
+            {
+                options.KeyPrefix = "ABPEcommerce:";
+            });
+            var dataProtectionBuilder = context.Services.AddDataProtection().SetApplicationName("ABPEcommerce");
+            if (!hostEnvironment.IsDevelopment())
+            {
+                var redis = ConnectionMultiplexer.Connect(configuration["Redis:Configuration"]);
+                dataProtectionBuilder.PersistKeysToStackExchangeRedis(redis, "ABPEcommerce-Protection-Keys");
+            }
+
+        }
+
+        public override void OnApplicationInitialization(ApplicationInitializationContext context)
+        {
+            context.AddBackgroundWorkerAsync<EmailMarketingWorker>();
+        }
+    }
+}
